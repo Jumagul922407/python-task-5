67324ca78055a5388333692c088f0bb8211bb915
Jumagul <kultaevajumagul@gmail.com>
2025-05-15 07:13:57 +0600

    Add task5_loops.py for task 5

---
+++ task5_loops.py
@@ -0,0 +1,12 @@
+
+# Python Task 5: Cycles (Loops)
+
+# For loop example: Printing numbers 1 to 5
+for i in range(1, 6):
+    print(f"Number: {i}")
+
+# While loop example: Printing numbers 1 to 5
+i = 1
+while i <= 5:
+    print(f"Number: {i}")
+    i += 1
